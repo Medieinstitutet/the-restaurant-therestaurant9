6371e8284e60246032b8d9a858bbebdb7c312350
Marcus Gustavsson <mackan1892@outlook.com>
2024-02-12 16:08:06 +0100

    Base layout startpage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,13 @@
+import "./../styles/_home.scss";
+
 export const Home = () => {
-  return <>Home</>;
+  return (
+    <>
+      <main className="hero_Image">
+        <section className="hero_text">
+          <p>Välkommen till Trattoria Gustoso</p>
+        </section>
+      </main>
+    </>
+  );
 };
