7f2bde7fb2ef03ccdfac21f05e9a55a8101d0f4a
Erikli15 <erik.lindner@workmail.com>
2024-02-13 14:43:58 +0100

    Menu

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,3 +1,26 @@
 export const Menu = () => {
-  return <>Menu</>;
+  return (
+    <>
+      <section className="menu-container">
+        <h2>Menyn</h2>
+        <article className="menu">
+          <span className="menu1">
+            <img src="/public/Meny.jpg" alt="Meny1" />
+          </span>
+          <span className="menu2">
+            <img src="/public/Meny2.jpg" alt="Meny2" />
+          </span>
+        </article>
+        <h3>Information av menyn</h3>
+        <article className="menu-info">
+          <span className="menyinfo1">
+            <img src="/public/infomeny1.jpg" alt="Meny information 1" />
+          </span>
+          <span className="menyinfo2">
+            <img src="/public/infomeny2.jpg" alt="Meny information 2" />
+          </span>
+        </article>
+      </section>
+    </>
+  );
 };
