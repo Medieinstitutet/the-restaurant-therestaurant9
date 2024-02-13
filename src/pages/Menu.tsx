8c9ccb279a1b97f6ca3a0fe756cb298cfa7595a7
Erikli15 <erik.lindner@workmail.com>
2024-02-13 15:21:01 +0100

    ändra sök värd

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -5,19 +5,19 @@ export const Menu = () => {
         <h2>Menyn</h2>
         <article className="menu">
           <span className="menu1">
-            <img src="/public/Meny.jpg" alt="Meny1" />
+            <img src="/Meny.jpg" alt="Meny1" />
           </span>
           <span className="menu2">
-            <img src="/public/Meny2.jpg" alt="Meny2" />
+            <img src="/Meny2.jpg" alt="Meny2" />
           </span>
         </article>
         <h3>Information av menyn</h3>
         <article className="menu-info">
           <span className="menyinfo1">
-            <img src="/public/infomeny1.jpg" alt="Meny information 1" />
+            <img src="/infomeny1.jpg" alt="Meny information 1" />
           </span>
           <span className="menyinfo2">
-            <img src="/public/infomeny2.jpg" alt="Meny information 2" />
+            <img src="/infomeny2.jpg" alt="Meny information 2" />
           </span>
         </article>
       </section>
