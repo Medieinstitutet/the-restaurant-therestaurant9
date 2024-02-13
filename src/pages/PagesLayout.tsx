6c7c43ff761e366395830c8e6d780349365b3751
SanneJacobsson <sanne.jacobsson@gmail.com>
2024-02-13 15:48:00 +0100

    navigation component

--- src/pages/PagesLayout.tsx
+++ src/pages/PagesLayout.tsx
@@ -1,26 +1,11 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { Navigation } from "../components/Navigation";
 
 export const PagesLayout = () => {
   return (
     <>
       <header>
-        <nav>
-          <h2>Trattoria Gustoso</h2>
-          <ul>
-            <li>
-              <NavLink to={"/"}>Hem</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/pages/booking"}>Reservation</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/pages/menu"}>Meny</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/pages/contact"}>Kontakt</NavLink>
-            </li>
-          </ul>
-        </nav>
+        <Navigation />
       </header>
       <main>
         <Outlet />
