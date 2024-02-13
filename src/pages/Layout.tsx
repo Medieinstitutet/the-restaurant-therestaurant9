11406ef1b4890cc9b8c1c95b4633a28384be7d13
Marcus Gustavsson <mackan1892@outlook.com>
2024-02-13 12:33:04 +0100

    Navbar

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,3 +1,4 @@
+import { Link, NavLink } from "react-router-dom";
 import "./../styles/App.scss";
 
 const restaurtantId = "65cb31932b1f9164881776d0";
@@ -5,14 +6,40 @@ const restaurtantId = "65cb31932b1f9164881776d0";
 export const Layout = () => {
   return (
     <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to={"/"}>Hem</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/pages/booking"}>Reservation</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/pages/menu"}>Meny</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/pages/contact"}>Kontakt</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <div className="hero_Image">
         <div>
           <section className="hero_text"></section>
-          <p>Välkommen till Trattoria Gustoso</p>
+          <p className="restaurant_text">Välkommen till Trattoria Gustoso</p>
         </div>
       </div>
-      <div className="startpage_meny">Meny</div>
-      <div className="reservation_container">Reservation</div>
+      <div className="startpage_meny">
+        <button>
+          <NavLink to={"/pages/menu"}>Meny</NavLink>
+        </button>
+      </div>
+      <div className="reservation_container">
+        <button>
+          <Link to={"/pages/booking"}>Reservation</Link>
+        </button>
+      </div>
     </>
   );
 };
--- src/pages/PagesLayout.tsx
+++ src/pages/PagesLayout.tsx
@@ -1,9 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export const PagesLayout = () => {
   return (
     <>
-      <header></header>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to={"/"}>Hem</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/pages/booking"}>Reservation</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/pages/menu"}>Meny</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/pages/contact"}>Kontakt</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <main>
         <Outlet />
       </main>
