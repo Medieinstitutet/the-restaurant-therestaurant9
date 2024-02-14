01c78622e99dc30b90b0893ca7ffee8f4ac968d2
Marcus Gustavsson <mackan1892@outlook.com>
2024-02-14 15:59:19 +0100

    Styling startpage and footer

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,28 +1,28 @@
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./../styles/App.scss";
 
 const restaurtantId = "65cb31932b1f9164881776d0";
 
-function reveal() {
-  var reveals = document.querySelectorAll(".startpage_meny");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-
-window.addEventListener("scroll", reveal);
-
 export const Layout = () => {
   const navigate = useNavigate();
+
+  function reveal() {
+    var reveals = document.querySelectorAll(".startpage_meny");
+
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
 
   return (
     <>
@@ -48,7 +48,10 @@ export const Layout = () => {
       <div className="hero_Image">
         <div>
           <section className="hero_shadow"></section>
-          <p className="restaurant_text">Välkommen till Trattoria Gustoso</p>
+          <div className="restaurant_text">
+            <p>Välkommen till</p>
+            <p>Trattoria Gustoso</p>
+          </div>
         </div>
       </div>
       <div className="startpage_meny">
@@ -61,23 +64,53 @@ export const Layout = () => {
           av autentiska smaker och en måltid som är full av kärlek och passion
           för matlagning. Buon appetito!"
         </p>
+
         <button
           onClick={() => {
             navigate("/pages/menu");
           }}
         >
-          Meny
+          Visa hela menyn
         </button>
       </div>
-      <div className="reservation_container">
+      <div className="reservation">
+        <div className="reservation_container">
+          <img src="src\assets\OIP.jpg" alt="Italian food" className="" />
+          <p>
+            "Oavsett om det är en romantisk middag för två, en familjefest eller
+            en affärslunch, ser vi fram emot att välkomna dig till vårt bord och
+            skapa minnen som varar livet ut. Boka ditt bord idag och låt oss ta
+            hand om resten. Din resa till Italien börjar här! Kontakta oss nu
+            för att göra din reservation och upptäck varför Trattoria Gustoso är
+            destinationen för finsmakare och matälskare överallt.""
+          </p>
+          <button
+            onClick={() => {
+              navigate("/pages/booking");
+            }}
+          >
+            Boka bord
+          </button>
+        </div>
+      </div>
+      <footer>
+        <section>
+          <h3>Öppettider:</h3>
+          <p>Onsdag-Söndag: 17-23</p>
+        </section>
+        <section>
+          <h3>Adress:</h3>
+          <p>Drottninggatan 1</p>
+          <p>123 45 Stockholm</p>
+        </section>
         <button
           onClick={() => {
-            navigate("/pages/booking");
+            navigate("/pages/contact");
           }}
         >
-          Reservation
+          Kontakta oss
         </button>
-      </div>
+      </footer>
     </>
   );
 };
