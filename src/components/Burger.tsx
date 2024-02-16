746cd6f16c54bc08e52f1a649e8733002a2bb2fa
Marcus Gustavsson <mackan1892@outlook.com>
2024-02-16 17:41:04 +0100

    Hamburger menu

---
+++ src/components/Burger.tsx
@@ -0,0 +1,46 @@
+import react, { useEffect, useRef, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+export const Burger = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <>
+      <div className="mobile-burger-button">
+        <button
+          type="button"
+          id="burger-menu"
+          onClick={() => setNavbarOpen(!navbarOpen)}
+        >
+          <span className="burger-bar-one"></span>
+          <span className="burger-bar-two"></span>
+          <span className="burger-bar-three"></span>
+        </button>
+      </div>
+      <div className={`menu-nav${navbarOpen ? " show-menu" : ""}`}>
+        <ul>
+          <li>
+            <NavLink to={"/"} onClick={() => setNavbarOpen(false)}>
+              Hem
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/pages/booking"} onClick={() => setNavbarOpen(false)}>
+              Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/pages/menu"} onClick={() => setNavbarOpen(false)}>
+              Meny
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/pages/contact"} onClick={() => setNavbarOpen(false)}>
+              Kontakt
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
